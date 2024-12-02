3495e27f1846f982aeb787f57d4a75dea76bb20a
LamKien <luonglamkien@gmail.com>
2024-12-02 18:10:52 +0700

    Runner

--- rpy/runner.py
+++ rpy/runner.py
@@ -1,41 +1,46 @@
 from rpy.motor import MOTOR1,MOTOR2
-
 import machine #type: ignore
 import rpy.hw.hmc5883l as hmc5883l
 import time
 
 class ADC(machine.ADC):
-    LOW=0
-    MID=1
-    HIGH=2
-
-    def __init__(self, pin,sample):
+    class STATE:
+        LOW=0
+        MID=1
+        HIGH=2
+    class TYPE:
+        TYPE_1=0
+        TYPE_2=1
+    def __init__(self, pin,sample,type=TYPE.TYPE_1):
         super().__init__(machine.Pin(pin))
         self.atten(machine.ADC.ATTN_11DB)
         self.sample=sample
-
+        self._type=type
+    def read(self):
+        value=super().read()
+        return 4095-value if self._type==self.TYPE.TYPE_1 else value 
     def state(self):
-        return self.HIGH if self.read()<self.sample[0] else self.LOW if self.read()>self.sample[0] else self.MID
+        return self.HIGH if self.read()>self.sample[1] else self.LOW if self.read()<self.sample[0] else self.MID
 
 class ADC1(ADC):
-    def __init__(self,sample=(200,400)):
+    def __init__(self,sample=(1800,2000)):
         super().__init__(33,sample)
 
 class ADC2(ADC):
-    def __init__(self,sample=(200,400)):
+    def __init__(self,sample=(1700,2000)):
         super().__init__(34,sample)
 
 class ADC3(ADC):
-    def __init__(self,sample=(200,400)):
+    def __init__(self,sample=(1700,2000)):
         super().__init__(35,sample)
 
 class ADC4(ADC):
-    def __init__(self,sample=(200,400)):
+    def __init__(self,sample=(1700,2000)):
         super().__init__(36,sample)
 
 class RUNNER: 
     __offset__=(0,0)
-    __speed__=(300,700,900,1000)
+    __speed__=(450,700,900,1000)
     def __init__(self):
         """ RUNNER là thư viện thay thế cho thư viện dò line 
             Các tham số cơ bản vẫn giữ nguyên, có thêm tham số về là bàn số
@@ -75,20 +80,35 @@ class RUNNER:
         while True:
             try:
                if self.adcs[0].state()==ADC.HIGH and self.adcs[3].state()==ADC.HIGH:
+                   self.motor1.stop()
+                   self.motor2.stop()
+                   return True
+               if self.adcs[1].state()==ADC.HIGH and self.adcs[2].state()==ADC.HIGH:
+                   self.motor1.run(self.speed[2])
+                   self.motor2.run(self.speed[2])
+               else:
+                   if self.adcs[1].state()==ADC.LOW:
+                        self.motor1.run(self.speed[3])
+                        if self.adcs[3].state()==ADC.HIGH:
+                            self.motor2.run(self.speed[0])
+                        else:
+                            self.motor2.run(self.speed[1])
+                   if self.adcs[2].state()==ADC.LOW:
+                        self.motor2.run(self.speed[3])
+                        if self.adcs[0].state()==ADC.HIGH:
+                            self.motor1.run(self.speed[0])
+                        else:
+                            self.motor1.run(self.speed[1])
+               if self.adcs[0].state()==ADC.LOW and self.adcs[1].state()==ADC.LOW and self.adcs[2].state()==ADC.LOW and self.adcs[3].state()==ADC.LOW:
+                    self.motor1.stop()
+                    self.motor2.stop()
+                    return False
             except Exception as e:
-                print('false')
-                run=False
+                print('false: ',e.__str__())
                 self.motor1.stop()
                 self.motor2.stop()
                 return False
-        self.motor1.stop()
-        self.motor2.stop()
-        return True
-
-    def _run_steps(self,step=1):
+    def run_steps(self,step=1):
         self.run=True
         for _ in range(step):
-            self._run_step()
-
-    def run_steps(self,step=1):
-        self._run_steps(step)
+            self.run_step()
