fa731789d1c73a0c7aec6217d12f66e8babafb46
ame <kayori@waifu.club>
2024-12-02 17:46:18 +0700

    fixup! rpy: runner: adapt to 7e2de33e81f2418e4f8ba7aed85895484d257bce and misc changes

--- rpy/runner.py
+++ rpy/runner.py
@@ -1,32 +1,39 @@
-
 from rpy.motor import MOTOR1,MOTOR2
+
 import machine #type: ignore
-import rpy.hmc5883l as hmc5883l
+import rpy.hw.hmc5883l as hmc5883l
 import time
 
 class ADC(machine.ADC):
     LOW=0
     MID=1
     HIGH=2
+
     def __init__(self, pin,sample):
         super().__init__(machine.Pin(pin))
         self.atten(machine.ADC.ATTN_11DB)
         self.sample=sample
+
     def state(self):
         return self.HIGH if self.read()<self.sample[0] else self.LOW if self.read()>self.sample[0] else self.MID
+
 class ADC1(ADC):
     def __init__(self,sample=(200,400)):
         super().__init__(33,sample)
+
 class ADC2(ADC):
     def __init__(self,sample=(200,400)):
         super().__init__(34,sample)
+
 class ADC3(ADC):
     def __init__(self,sample=(200,400)):
         super().__init__(35,sample)
+
 class ADC4(ADC):
     def __init__(self,sample=(200,400)):
         super().__init__(36,sample)
-class RUNER: 
+
+class RUNNER: 
     __offset__=(0,0)
     __speed__=(300,700,900,1000)
     def __init__(self):
@@ -38,6 +45,7 @@ class RUNER:
         self.motor2=MOTOR1(self.__offset__[1])
         self.compass=hmc5883l.HMC5883L(scl=22,sda=21)
         self.compass.auto_update_declination()
+
     def _Turn(self,angle):
         """ Góc quay sang phải mang chiều dương """
         if angle>0:
@@ -62,7 +70,7 @@ class RUNER:
                 angle=abs(_angle_move-_angle)
         self.motor1.stop()
         self.motor2.stop()
-            
+
     def run_step(self):
         while True:
             try:
@@ -76,9 +84,11 @@ class RUNER:
         self.motor1.stop()
         self.motor2.stop()
         return True
+
     def _run_steps(self,step=1):
         self.run=True
         for _ in range(step):
             self._run_step()
+
     def run_steps(self,step=1):
         self._run_steps(step)
